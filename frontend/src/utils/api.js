f54a8c36e3ec6979ec4fedcf8ae36b85345b6f18
malla teja akash <you@example.com>
2026-05-08 17:20:55 +0530

    Hardcoded correct backend URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
     "https://certificate-generator-2-3qsf.onrender.com",
 });
 
-// CREATE CERTIFICATE
 export const createCertificate = async (data) => {
   const response = await API.post(
     "/api/certificates",
@@ -15,7 +13,6 @@ export const createCertificate = async (data) => {
   return response.data;
 };
 
-// GET SINGLE CERTIFICATE
 export const getCertificateById = async (id) => {
   const response = await API.get(
     `/api/certificates/${id}`
@@ -23,7 +20,6 @@ export const getCertificateById = async (id) => {
   return response.data;
 };
 
-// GET ALL CERTIFICATES
 export const getAllCertificates = async () => {
   const response = await API.get(
     "/api/certificates"
@@ -31,7 +27,6 @@ export const getAllCertificates = async () => {
   return response.data;
 };
 
-// DELETE CERTIFICATE
 export const deleteCertificate = async (id) => {
   const response = await API.delete(
     `/api/certificates/${id}`
